97d36438dc220727ef0d4827d67aa8b6cb60d912
Squirrelooooop <yanlin.s.liu@gmail.com>
2025-11-14 14:37:50 -0800

    video output for M4_5 40x Test

--- aqua_cmd_batch.m
+++ aqua_cmd_batch.m
@@ -15,8 +15,8 @@ close all;
 clc;
 clearvars
 startup;  % initialize
-pIn = '/Users/sunny/Desktop/Data/20251028_v35_m2/merged/'; %% input file folder
-pOut = '/Users/sunny/Desktop/Data/20251028_v35_m2/merged/'; %% the folder for output results. Note that it ends with \.
+pIn = '/Users/sunny/Desktop/Data/20251111_v35_m4_5_inv/merged/'; %% input file folder
+pOut = '/Users/sunny/Desktop/Data/20251111_v35_m4_5_inv/merged/'; %% the folder for output results. Note that it ends with \.
 
 batchSet.propMetric = true;    % whether extract propagation-related features
 batchSet.networkFeatures = true; % whether extract network features
@@ -50,7 +50,7 @@ files = [files_tif; files_tiff; files_mat];
 for xxx = 1:numel(files)
     f1 = files(xxx).name; 
     %% load setting (you can also manually modify setting here)
-    opts = util.parseParam_for_batch(3);
+    opts = util.parseParam_for_batch(4);
     opts.singleChannel = true;      % batch only leverages single channel for simplicity
     opts.whetherExtend = true;
     opts.detectGlo = false;
@@ -64,8 +64,8 @@ for xxx = 1:numel(files)
     opts.singleChannel = isempty(datOrg2);
 
     %% Remove the high freq noise by averaging 1 sec window pixel by pixel
-    Fs = 1/0.06;
-    tmp = reshape(datOrg1,[],1,1,5000);
+    Fs = 1/0.12; % 60ms per frame
+    tmp = reshape(datOrg1,[],1,1,1000); % how many frames in total
     clearvars dat0rg1
     for ipix = 1:size(tmp,1)
         tmp(ipix,:,:,:) = movmean(double(tmp(ipix,:,:,:)),Fs);
